0d0e70d8cc718086511ad39a5256bd6d5104f90e
Luiz Felipe <alucardlf1911@gmail.com>
2025-11-18 15:08:48 -0300

    Criando class Position

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+import org.example.boardgame.Position;
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Position position = new Position(3, 5);
 
+        System.out.println(position);
     }
 }
---
+++ src/main/java/org/example/boardgame/Position.java
@@ -0,0 +1,41 @@
+package org.example.boardgame;
+
+public class Position
+{
+    private Integer row;
+    private Integer column;
+
+    public Position() {
+
+    }
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(row + ", " + column);
+        return sb.toString();
+    }
+}
+
