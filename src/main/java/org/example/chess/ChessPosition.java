dfb4851c9941c486e832e907482833e3489d848b
Luiz Felipe <alucardLf1911@gmail.com>
2025-11-22 10:54:27 -0300

    Criando Exceção de Chess

---
+++ src/main/java/org/example/chess/ChessPosition.java
@@ -0,0 +1,40 @@
+package org.example.chess;
+
+import org.example.boardgame.Position;
+
+public class ChessPosition {
+
+    private char column;
+    private int row;
+
+    public ChessPosition() {}
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessExceptions("Erro: posição inválida, aceita somente a1 até h8");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition() {
+        return new Position(8 - this.row, this.column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char) ('a' - position.getColumn()),8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
