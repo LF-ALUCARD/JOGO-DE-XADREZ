806b278d172dd4feb2c25d0e931d3b3c53f9006d
Luiz Felipe <alucardLf1911@gmail.com>
2025-11-22 10:24:14 -0300

    Criando Exceção de Board

--- src/main/java/org/example/boardgame/Board.java
+++ src/main/java/org/example/boardgame/Board.java
@@ -7,6 +7,10 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(Integer row, Integer column) {
+        if (row < 1 || column < 1){
+            throw new BoardExceptions("Erro: O tabuleiro dve ser maior que 1X1");
+        }
+
         this.row = row;
         this.column = column;
         pieces = new Piece[row][column];
@@ -16,28 +20,44 @@ public class Board {
         return row;
     }
 
-    public void setRow(Integer row) {
-        this.row = row;
-    }
-
     public Integer getColumn() {
         return column;
     }
 
-    public void setColumn(Integer column) {
-        this.column = column;
-    }
-
     public Piece piece(Integer row, Integer column){
+        if (!positionExists(row, column)){
+            throw new BoardExceptions("Erro: Essa posiação não existe");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position){
+        if (!positionExists(position)){
+            throw new BoardExceptions("Erro: Essa posiação não existe");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece (Piece piece, Position position){
+        if (thereIsPiece(position)){
+            throw new BoardExceptions("Erro: já existe uma peça nessa posição");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public boolean positionExists(int row, int column){
+        return row >= 0 && row <= this.row && column >= 0 && column <= this.column;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsPiece(Position position){
+        if (!positionExists(row, column)){
+            throw new BoardExceptions("Erro: Essa posiação não existe");
+        }
+        return piece(position) != null;
     }
 }
